b00d35699c2f7a00774eb3ffcb46ae80b2d8e99c
tcd159 <tcd159@naver.com>
2019-08-20 14:24:40 +0900

    반복문 커밋

--- Project1/1.cpp
+++ Project1/1.cpp
@@ -2,8 +2,16 @@
 
 int main()
 {
-		printf("Hello World");
-		
-		return 0;
+	int count = 5;
+
+	for (int i = 0; i < count; i++)
+	{
+		for (int j = 0; j < i+1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
 
 }
