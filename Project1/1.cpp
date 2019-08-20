0e64056703a6c1291b45a61ae129ae29e866aae7
tcd159 <tcd159@naver.com>
2019-08-20 14:38:43 +0900

    반복문 커밋2

--- Project1/1.cpp
+++ Project1/1.cpp
@@ -1,17 +1,23 @@
 #include <stdio.h>
+//*****
+//****
+//***
+//**
+//*
 
 int main()
 {
-	int count = 5;
-
-	for (int i = 0; i < count; i++)
+	for (int j = 0; j < 5;j++)
 	{
-		for (int j = 0; j < i+1; j++)
+		for (int i = 0; i < 5 - j; i++)
 		{
 			printf("*");
 		}
 		printf("\n");
 	}
+	
+		
+
 	return 0;
 
 }
